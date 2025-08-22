54df50b4f5ff364e503800cc82a8db13916c1862
Liz-nor <lise-ervik@hotmail.com>
2025-08-22 23:43:56 +0200

    getting started

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector("#container");
+const API_URL = "https://v2.api.noroff.dev/square-eyes";
+
+async function fetchAndCreateMovies() {
+  try {
+    const respons = await fetch(API_URL);
+    const data = await respons.json();
+    const movies = data.data;
+
+    movies.forEach((product) => {
+      const card = document.createElement("div");
+      const image = document.createElement("img");
+      const content = document.createElement("div");
+      const title = document.createElement("h2");
+      const price = document.createElement("p");
+
+      card.className = "card";
+      image.className = "card-image";
+      content.className = "card-content";
+      title.className = "card-title";
+      price.className = "card-price";
+
+      image.src = product.image.url;
+      image.alt = product.image.alt;
+      title.textContent = product.title;
+      price.textContent = product.price;
+
+      content.appendChild(title);
+      content.appendChild(price);
+      card.appendChild(image);
+      card.appendChild(content);
+
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Failed to load");
+  }
+}
+
+fetchAndCreateMovies();
